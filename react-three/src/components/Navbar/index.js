867f703ee78b1c7fc890be00fe10fc9906bc0aff
hborawar <himanshiborawar98@gmail.com>
2022-08-02 14:31:04 +0530

    first commit

---
+++ react-three/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const NavBar = () => {
+  return (
+    <>
+     <Nav>
+        <NavBarContainer>
+            <NavLogo>dolla</NavLogo>
+        </NavBarContainer>
+     </Nav>
+    </>
+  )
+}
+
+export default NavBar;
